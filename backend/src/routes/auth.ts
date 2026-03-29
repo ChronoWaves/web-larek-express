129a8195b7492b24a2997d26d231ccdd9e769e8c
ChronoWave <ChronoWave@yandex.ru>
2026-03-29 17:15:02 +0300

    feat: add routes, set up app entry point

---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import {
+  login,
+  register,
+  refreshAccessToken,
+  logout,
+  getCurrentUser,
+} from '../controllers/auth';
+import { validateLogin, validateRegister } from '../middlewares/validators';
+import auth from '../middlewares/auth';
+
+const authRouter = Router();
+
+authRouter.post('/login', validateLogin, login);
+authRouter.post('/register', validateRegister, register);
+authRouter.get('/token', refreshAccessToken);
+authRouter.get('/logout', logout);
+authRouter.get('/user', auth, getCurrentUser);
+
+export default authRouter;
